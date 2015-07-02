61d2208a80f9922e429b4d684055014d62d30390
Benjamin Thurm <bentrm@posteo.de>
2015-07-02 13:22:38 +0200

    Output projection with geometry where applicable

--- openinfra_core/src/main/java/de/btu/openinfra/backend/db/pojos/AttributeValueGeomType.java
+++ openinfra_core/src/main/java/de/btu/openinfra/backend/db/pojos/AttributeValueGeomType.java
@@ -16,26 +16,39 @@ public enum AttributeValueGeomType {
 	 * Well-known text (WTK) representation of the geometry type
 	 * (@see http://en.wikipedia.org/wiki/Well-known_text).
 	 */
-	TEXT,
+	TEXT	("ST_asText(geom)"),
 	/**
 	 * The Keyhole Markup Language (KML) representation of the geometry type 
 	 * (@see http://de.wikipedia.org/wiki/Keyhole_Markup_Language).
 	 */
-	KML,
+	KML		("ST_asKML(geom)"),
 	/**
 	 * The Geography Markup Language (GML) representation of the geometry type 
 	 * (@see http://de.wikipedia.org/wiki/Geography_Markup_Language).
 	 */
-	GML,
+	GML		("ST_asGML(geom)"),
 	/**
 	 * The GeoJSON representation of the geometry type 
 	 * (@see http://geojson.org/).
+	 *
+	 * Signature to get GeoJSON String with precision of up to 15 decimals and
+	 * with designated projection identifier in long CRS notation (option 4).
 	 */
-	GEOJSON,
+	GEOJSON	("ST_asGeoJSON(geom, 15, 3)"),
 	/**
 	 * The X3D representation of the geometry type 
 	 * (@see http://de.wikipedia.org/wiki/X3D).
 	 */
-	X3D
+	X3D		("ST_asX3D(geom)");
+
+	private String psqlFnSignature;
+
+	private AttributeValueGeomType(String psqlFnSignature) {
+		this.psqlFnSignature = psqlFnSignature;
+	}
+
+	public String getPsqlFnSignature() {
+		return this.psqlFnSignature;
+	}
 
 }
