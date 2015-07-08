b05d15dda07bcad5e11abd4afc872f7b7695c218
noacktino <noacktin@b-tu.de>
2015-07-08 10:17:01 +0200

    created enumeration for sorting

---
+++ openinfra_core/src/main/java/de/btu/openinfra/backend/db/daos/OpenInfraOrderBy.java
@@ -0,0 +1,19 @@
+package de.btu.openinfra.backend.db.daos;
+
+/**
+ * OpenInfraOrderBy is an enumeration of the possible sort orderings.
+ * 
+ * @author <a href="http://www.b-tu.de">BTU</a> DBIS
+ *
+ */
+public enum OpenInfraOrderBy {
+	
+	/**
+	 * Enumeration value indicating the items are sorted in increasing order.
+	 */
+	ASC,
+	/**
+	 * Enumeration value indicating the items are sorted in decreasing order.
+	 */
+	DESC
+}
