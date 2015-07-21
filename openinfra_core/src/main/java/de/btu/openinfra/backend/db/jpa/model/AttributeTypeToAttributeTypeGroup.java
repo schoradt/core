583602d72e7d5f6c5a19562a89f0eb3c0843fb95
Philipp Egert <egert@b-tu.de>
2015-07-21 13:07:15 +0200

    Add functionality

--- openinfra_core/src/main/java/de/btu/openinfra/backend/db/jpa/model/AttributeTypeToAttributeTypeGroup.java
+++ openinfra_core/src/main/java/de/btu/openinfra/backend/db/jpa/model/AttributeTypeToAttributeTypeGroup.java
@@ -31,16 +31,22 @@ import javax.persistence.Table;
 			query="SELECT a "
 					+ "FROM AttributeTypeToAttributeTypeGroup a "
 					+ "WHERE a.attributeTypeGroup = :value"),
+	@NamedQuery(name="AttributeTypeToAttributeTypeGroup."
+			+ "findByAttributeTypeGroupAndAttributeType",
+			query="SELECT a "
+					+ "FROM AttributeTypeToAttributeTypeGroup a "
+					+ "WHERE a.attributeTypeGroup = :value AND "
+					+ "a.attributeType = :value2"),
 	@NamedQuery(name="AttributeTypeToAttributeTypeGroup.findByAttributeType",
 			query="SELECT a "
 					+ "FROM AttributeTypeToAttributeTypeGroup a "
 					+ "WHERE a.attributeType = :value"),
 	@NamedQuery(name="AttributeTypeToAttributeTypeGroup."
 			+ "findByAttributeTypeAndAttributeTypeGroup",
-	query="SELECT a "
-			+ "FROM AttributeTypeToAttributeTypeGroup a "
-			+ "WHERE a.attributeType = :value AND "
-			+ "a.attributeTypeGroup = :value2"),
+			query="SELECT a "
+					+ "FROM AttributeTypeToAttributeTypeGroup a "
+					+ "WHERE a.attributeType = :value AND "
+					+ "a.attributeTypeGroup = :value2"),
 	@NamedQuery(name="AttributeTypeToAttributeTypeGroup.countByAttributeType",
 		query="SELECT COUNT(a) "
 				+ "FROM AttributeTypeToAttributeTypeGroup a "
