6038b1fb27e0b9a249c41a7036faa67503d963f4
xa2kx <schulal2@b-tu.de>
2015-07-21 14:41:36 +0200

    added new AttributValueGeomTypes for writing geometry data

--- openinfra_core/src/main/java/de/btu/openinfra/backend/db/daos/AttributeValueGeomType.java
+++ openinfra_core/src/main/java/de/btu/openinfra/backend/db/daos/AttributeValueGeomType.java
@@ -3,32 +3,32 @@ package de.btu.openinfra.backend.db.daos;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
- * This enumeration describes possible output formats for the data type 
+ * This enumeration describes possible output formats for the data type
  * geometry.
- * 
+ *
  * @author <a href="http://www.b-tu.de">BTU</a> DBIS
  *
  */
 @XmlRootElement
 public enum AttributeValueGeomType {
-	
+
 	/**
 	 * Well-known text (WTK) representation of the geometry type
 	 * (@see http://en.wikipedia.org/wiki/Well-known_text).
 	 */
 	TEXT	("ST_asText(geom)"),
 	/**
-	 * The Keyhole Markup Language (KML) representation of the geometry type 
+	 * The Keyhole Markup Language (KML) representation of the geometry type
 	 * (@see http://de.wikipedia.org/wiki/Keyhole_Markup_Language).
 	 */
 	KML		("ST_asKML(geom)"),
 	/**
-	 * The Geography Markup Language (GML) representation of the geometry type 
+	 * The Geography Markup Language (GML) representation of the geometry type
 	 * (@see http://de.wikipedia.org/wiki/Geography_Markup_Language).
 	 */
 	GML		("ST_asGML(geom)"),
 	/**
-	 * The GeoJSON representation of the geometry type 
+	 * The GeoJSON representation of the geometry type
 	 * (@see http://geojson.org/).
 	 *
 	 * Signature to get GeoJSON String with precision of up to 15 decimals and
@@ -36,10 +36,34 @@ public enum AttributeValueGeomType {
 	 */
 	GEOJSON	("ST_asGeoJSON(geom, 15, 3)"),
 	/**
-	 * The X3D representation of the geometry type 
+	 * The X3D representation of the geometry type
 	 * (@see http://de.wikipedia.org/wiki/X3D).
 	 */
-	X3D		("ST_asX3D(geom)");
+	X3D		("ST_asX3D(geom)"),
+
+	/**
+	 * The geometry representation of a well-known text (WTK) type
+	 * (@see http://en.wikipedia.org/wiki/Well-known_text).
+	 */
+	FROM_TEXT    ("ST_GeomFromText"),
+	/**
+     * The geometry representation of the Keyhole Markup Language (KML) type
+     * (@see http://de.wikipedia.org/wiki/Keyhole_Markup_Language).
+     */
+	FROM_KML     ("ST_GeomFromKML"),
+	/**
+     * The geometry representation of the Geography Markup Language (GML) type
+     * (@see http://de.wikipedia.org/wiki/Geography_Markup_Language).
+     */
+	FROM_GML     ("ST_GeomFromGML"),
+	/**
+     * The geometry representation of the GeoJSON type
+     * (@see http://geojson.org/).
+     *
+     * Signature to get GeoJSON String with precision of up to 15 decimals and
+     * with designated projection identifier in long CRS notation (option 4).
+     */
+	FROM_GEOJSON ("ST_GeomFromGeoJSON");
 
 	private String psqlFnSignature;
 
