159c0f4fe10800c7b4a7e9a8ba678e4664702b9c
xa2kx <schulal2@b-tu.de>
2015-08-05 08:52:03 +0200

    updated attribute type resources for POST, PUT and DELETE operations
    Added functionality for attribute type associations.

--- openinfra_core/src/main/java/de/btu/openinfra/backend/db/daos/AttributeTypeAssociationDao.java
+++ openinfra_core/src/main/java/de/btu/openinfra/backend/db/daos/AttributeTypeAssociationDao.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import de.btu.openinfra.backend.db.jpa.model.AttributeType;
 import de.btu.openinfra.backend.db.jpa.model.AttributeTypeXAttributeType;
+import de.btu.openinfra.backend.db.jpa.model.ValueListValue;
 import de.btu.openinfra.backend.db.pojos.AttributeTypeAssociationPojo;
 
 /**
@@ -42,15 +43,36 @@ public class AttributeTypeAssociationDao
 
 	@Override
 	public MappingResult<AttributeTypeXAttributeType> mapToModel(
-			AttributeTypeAssociationPojo pojoObject,
-			AttributeTypeXAttributeType modelObject) {
-		// TODO Auto-generated method stub
-		return null;
+			AttributeTypeAssociationPojo pojo,
+			AttributeTypeXAttributeType atxat) {
+
+	    // return null if the pojo is null
+        if (pojo != null) {
+
+            // set the first attribute type
+            atxat.setAttributeType1Bean(em.find(
+                    AttributeType.class, pojo.getAssociationAttributeTypeId()));
+
+            // set the related attribute type
+            atxat.setAttributeType2Bean(em.find(
+                    AttributeType.class,
+                    pojo.getAssociatedAttributeType().getUuid()));
+
+            // set the relationship
+            atxat.setValueListValue(em.find(
+                    ValueListValue.class, pojo.getRelationship().getUuid()));
+
+            // return the model as mapping result
+            return new MappingResult<AttributeTypeXAttributeType>(
+                    atxat.getId(), atxat);
+        } else {
+            return null;
+        }
 	}
-	
+
 	/**
 	 * This method implements the method mapToPojo in a static way.
-	 * 
+	 *
 	 * @param locale       the requested language as Java.util locale
 	 * @param association  the model object
 	 * @return             the POJO object when the model object is not null
@@ -58,19 +80,28 @@ public class AttributeTypeAssociationDao
 	 */
 	public static AttributeTypeAssociationPojo mapToPojoStatically(
 			Locale locale,
-			AttributeTypeXAttributeType association) {
-		
-		if(association != null) {
+			AttributeTypeXAttributeType atxat) {
+
+		if(atxat != null) {
 			AttributeTypeAssociationPojo pojo =
 					new AttributeTypeAssociationPojo();
-			
-			pojo.setUuid(association.getId());
+
+			// set the id of the pojo
+			pojo.setUuid(atxat.getId());
+
+			// set the relationship type object
 			pojo.setRelationship(ValueListValueDao.mapToPojoStatically(locale,
-					association.getValueListValue()));
+			        atxat.getValueListValue()));
+
+			// set the association attribute type id
+			pojo.setAssociationAttributeTypeId(atxat.getAttributeType1Bean()
+			        .getId());
+
+			// set the associated attribute type object
 			pojo.setAssociatedAttributeType(
 				AttributeTypeDao.mapToPojoStatically(locale,
-				association.getAttributeType2Bean()));
-			
+				        atxat.getAttributeType2Bean()));
+
 			return pojo;
 		}
 		else {
@@ -78,4 +109,20 @@ public class AttributeTypeAssociationDao
 		}
 	}
 
+	/**
+     * This method creates a AttributeTypeAssociationPojo shell that contains
+     * informations about the attribute type the association starts from.
+     *
+     * @param attributeTypeId the attribute type id the association starts from
+     * @return                the AttributeTypePojo
+     */
+    public AttributeTypeAssociationPojo newAttributeTypeAssociation(
+            UUID attributeTypeId) {
+        // create the return pojo
+        AttributeTypeAssociationPojo pojo = new AttributeTypeAssociationPojo();
+
+        //pojo.set
+
+        return pojo;
+    }
 }
