46ac2c9e9008b2567c1137feea1bfe05443f13e8
Philipp Egert <egert@b-tu.de>
2015-08-11 11:07:01 +0200

    Add rest endpoints for '/metadata/settings' (POST, PUT, DELETE)

---
+++ openinfra_core/src/main/java/de/btu/openinfra/backend/db/pojos/meta/SettingKeysPojo.java
@@ -0,0 +1,17 @@
+package de.btu.openinfra.backend.db.pojos.meta;
+
+import de.btu.openinfra.backend.db.pojos.OpenInfraPojo;
+
+public class SettingKeysPojo extends OpenInfraPojo {
+    
+    private String key;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+}
