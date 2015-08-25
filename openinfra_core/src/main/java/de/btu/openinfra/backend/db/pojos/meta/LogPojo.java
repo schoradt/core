bdcfc7929d040fe549c0ae61bf258d71fa85c1a9
xa2kx <schulal2@b-tu.de>
2015-08-25 17:03:34 +0200

    set id and trid automatically for meta data schema POJOs

--- openinfra_core/src/main/java/de/btu/openinfra/backend/db/pojos/meta/LogPojo.java
+++ openinfra_core/src/main/java/de/btu/openinfra/backend/db/pojos/meta/LogPojo.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
+import de.btu.openinfra.backend.db.jpa.model.OpenInfraModelObject;
 import de.btu.openinfra.backend.db.pojos.OpenInfraPojo;
 
 @XmlRootElement
@@ -15,53 +16,61 @@ public class LogPojo extends OpenInfraPojo {
     private LoggerPojo logger;
     private LevelPojo level;
     private String message;
-    
+
+    /* Default constructor */
+    public LogPojo() {}
+
+    /* Constructor that will set the id, trid and meta data automatically */
+    public LogPojo(OpenInfraModelObject modelObject) {
+        super(modelObject);
+    }
+
     public UUID getUserId() {
         return userId;
     }
-    
+
     public void setUserId(UUID userId) {
         this.userId = userId;
     }
-    
+
     public String getUserName() {
         return userName;
     }
-    
+
     public void setUserName(String userName) {
         this.userName = userName;
     }
-    
+
     public String getCreatedOn() {
         return createdOn;
     }
-    
+
     public void setCreatedOn(String createdOn) {
         this.createdOn = createdOn;
     }
-    
+
     public LoggerPojo getLogger() {
         return logger;
     }
-    
+
     public void setLogger(LoggerPojo logger) {
         this.logger = logger;
     }
-    
+
     public LevelPojo getLevel() {
         return level;
     }
-    
+
     public void setLevel(LevelPojo level) {
         this.level = level;
     }
-    
+
     public String getMessage() {
         return message;
     }
-    
+
     public void setMessage(String message) {
         this.message = message;
     }
-    
+
 }
--- openinfra_core/src/main/java/de/btu/openinfra/backend/db/pojos/meta/LoggerPojo.java
+++ openinfra_core/src/main/java/de/btu/openinfra/backend/db/pojos/meta/LoggerPojo.java
@@ -2,19 +2,28 @@ package de.btu.openinfra.backend.db.pojos.meta;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
+import de.btu.openinfra.backend.db.jpa.model.OpenInfraModelObject;
 import de.btu.openinfra.backend.db.pojos.OpenInfraPojo;
 
 @XmlRootElement
 public class LoggerPojo extends OpenInfraPojo {
 
     private String logger;
-    
+
+    /* Default constructor */
+    public LoggerPojo() {}
+
+    /* Constructor that will set the id, trid and meta data automatically */
+    public LoggerPojo(OpenInfraModelObject modelObject) {
+        super(modelObject);
+    }
+
     public String getLogger() {
         return logger;
     }
-    
+
     public void setLogger(String logger) {
         this.logger = logger;
     }
-    
+
 }
