d492546f775ad7300ffc9cd812ecc20ccccfaa48
noacktino <noacktin@b-tu.de>
2015-09-09 10:00:32 +0200

    Extended documentation for filter

--- openinfra_core/src/main/java/de/btu/openinfra/backend/filter/OpenInfraRequestFilter.java
+++ openinfra_core/src/main/java/de/btu/openinfra/backend/filter/OpenInfraRequestFilter.java
@@ -10,6 +10,24 @@ import javax.ws.rs.core.MediaType;
 
 import org.apache.http.HttpHeaders;
 
+/**
+ * This class represents the request filter which is configured to be a 
+ * pre-matching filter. This is required since the filter must be accessed 
+ * before the available REST URLs are parsed.
+ * 
+ * Currently, this filter is used to replace the accept header and forward the
+ * the request to the origin URI when the requested URI ends with '.xml' or 
+ * '.json'. This helps to keep the REST API straight and consistent while the
+ * URI extension can be additionally provided.
+ * 
+ * There still exists another filter which is used to remove trailing slashes
+ * from the URI. This filter is still necessary since it is executed before this
+ * filter and directly redirects (including URL rewriting) to the origin URI. 
+ * This behavior cannot be achieved here.
+ * 
+ * @author <a href="http://www.b-tu.de">BTU</a> DBIS
+ *
+ */
 @PreMatching
 public class OpenInfraRequestFilter implements ContainerRequestFilter {
 
--- openinfra_core/src/main/java/de/btu/openinfra/backend/filter/package-info.java
+++ openinfra_core/src/main/java/de/btu/openinfra/backend/filter/package-info.java
@@ -1,4 +1,10 @@
 /**
+ * This package is used to hold ContainerRequestFilter or 
+ * ContainerResponseFilter classes.
+ * 
+ * Please read the documentation for more information:
+ * <a href="https://jersey.java.net/documentation/latest/filters-and-interceptors.html"/>
+ *  
  * @author <a href="http://www.b-tu.de">BTU</a> DBIS
  *
  */
