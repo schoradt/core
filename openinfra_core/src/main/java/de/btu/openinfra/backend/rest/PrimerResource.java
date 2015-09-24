3b5d85168da29c6529a908cad619f1bede22f425
Philipp <Philipp@141.43.177.220>
2015-09-24 17:20:25 +0200

    Add classes 'PojoPrimer' and 'PrimerResource'

---
+++ openinfra_core/src/main/java/de/btu/openinfra/backend/rest/PrimerResource.java
@@ -0,0 +1,21 @@
+package de.btu.openinfra.backend.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import de.btu.openinfra.backend.db.pojos.OpenInfraPojo;
+import de.btu.openinfra.backend.db.pojos.PojoPrimer;
+
+@Path(OpenInfraResponseBuilder.REST_URI_DEFAULT + "/primer")
+@Produces({ MediaType.APPLICATION_JSON + OpenInfraResponseBuilder.JSON_PRIORITY + OpenInfraResponseBuilder.UTF8_CHARSET,
+        MediaType.APPLICATION_XML + OpenInfraResponseBuilder.XML_PRIORITY + OpenInfraResponseBuilder.UTF8_CHARSET })
+public class PrimerResource {
+
+    @GET
+    public OpenInfraPojo primePojo(@QueryParam("pojoName") String pojoName) {
+        return PojoPrimer.primePojoStatically(pojoName);
+    }
+}
