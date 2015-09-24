b7fc04783a0b21a5a15f7d6752276159c8bdc394
Philipp <egertphi@b-tu.de>
2015-09-24 14:54:43 +0200

    Add new abstract method ('makePrimer')

--- openinfra_core/src/main/java/de/btu/openinfra/backend/db/pojos/OpenInfraPojo.java
+++ openinfra_core/src/main/java/de/btu/openinfra/backend/db/pojos/OpenInfraPojo.java
@@ -5,42 +5,47 @@ import java.util.UUID;
 import de.btu.openinfra.backend.db.jpa.model.OpenInfraModelObject;
 
 public abstract class OpenInfraPojo {
-	
-	private UUID uuid;
-	/**
-	 * This variable stands for the transaction id of the data object stored
-	 * in PostgreSQL in the xmin system column.
-	 */
-	private int trid;
-	
-	/**
-	 * The definition of the non-argument default constructor.
-	 */
-	protected OpenInfraPojo() {}
-	
-	/**
-	 * This constructor is used to set the id and the trid of the POJO object
-	 * automatically.
-	 * @param modelObject the current model object
-	 */
-	protected OpenInfraPojo(OpenInfraModelObject modelObject) {
-		this.uuid = modelObject.getId();
-		this.trid = modelObject.getXmin();
-	}
 
-	public UUID getUuid() {
-		return uuid;
-	}
+    private UUID uuid;
+    /**
+     * This variable stands for the transaction id of the data object stored in
+     * PostgreSQL in the xmin system column.
+     */
+    private int trid;
 
-	public void setUuid(UUID uuid) {
-		this.uuid = uuid;
-	}
-	
-	public int getTrid() {
-		return trid;
-	}
-	
-	public void setTrid(int trid) {
-		this.trid = trid;
-	}
+    /**
+     * The definition of the non-argument default constructor.
+     */
+    protected OpenInfraPojo() {
+    }
+
+    /**
+     * This constructor is used to set the id and the trid of the POJO object
+     * automatically.
+     * 
+     * @param modelObject
+     *            the current model object
+     */
+    protected OpenInfraPojo(OpenInfraModelObject modelObject) {
+        this.uuid = modelObject.getId();
+        this.trid = modelObject.getXmin();
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public int getTrid() {
+        return trid;
+    }
+
+    public void setTrid(int trid) {
+        this.trid = trid;
+    }
+
+    public abstract void makePrimer();
 }
