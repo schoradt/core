e8c063af7669a0462ac4953e46fc96c1bc53086f
noacktino <noacktin@b-tu.de>
2015-11-25 14:18:42 +0100

    introduced extended wadl export + javadoc

--- openinfra_core/src/main/java/de/btu/openinfra/backend/OpenInfraWadlConfig.java
+++ openinfra_core/src/main/java/de/btu/openinfra/backend/OpenInfraWadlConfig.java
@@ -16,21 +16,27 @@ import org.glassfish.jersey.server.wadl.internal.generators.resourcedoc.WadlGene
  * by the {@see OpenInfraApplication} class. We decided to place it in this
  * package next to where it is registered.
  *
+ * See:
+ * https://irfannagoo.wordpress.com/2012/12/20/jersey-extended-wadl-support/
+ * http://razvancaraghin.blogspot.de/2014/01/html-documentation-for-your-rest.html
+ * http://biemond.blogspot.de/2013/08/custom-jersey-wadl-generation.html
+ *
  * @author <a href="http://www.b-tu.de">BTU</a> DBIS
  *
  */
 public class OpenInfraWadlConfig extends WadlGeneratorConfig {
 
-	@Override
-	public List<WadlGeneratorDescription> configure() {
-        return generator(WadlGeneratorApplicationDoc.class)
-                .prop("applicationDocsStream", "application-doc.xml")
-                .generator(WadlGeneratorGrammarsSupport.class)
-                .prop("grammarsStream", "application-grammars.xml")
-                .prop("overrideGrammars", true)
-                .generator(WadlGeneratorResourceDocSupport.class)
-                .prop("resourceDocStream", "resourcedoc.xml")
-                .descriptions();
-	}
+    @Override
+    public List<WadlGeneratorDescription> configure() {
+
+        WadlGeneratorConfigDescriptionBuilder builder =
+        		generator(WadlGeneratorApplicationDoc.class)
+        		.prop("applicationDocsStream", "application-doc.xml")
+        		.generator(WadlGeneratorGrammarsSupport.class)
+        		.prop("grammarsStream", "application-grammars.xml")
+        		.generator(WadlGeneratorResourceDocSupport.class)
+        		.prop("resourceDocStream", "resourcedoc.xml");
+        return builder.descriptions();
+    }
 
 }
