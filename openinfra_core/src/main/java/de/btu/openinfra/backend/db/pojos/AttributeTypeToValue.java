7d21231beac72b9b5b03c517da534f6cbe89c5a3
noacktino <noacktin@b-tu.de>
2015-12-04 12:20:38 +0100

    issue #1585 -- added attributeTypeToAttributeTypeGroupId in topic

--- openinfra_core/src/main/java/de/btu/openinfra/backend/db/pojos/AttributeTypeToValue.java
+++ openinfra_core/src/main/java/de/btu/openinfra/backend/db/pojos/AttributeTypeToValue.java
@@ -1,11 +1,21 @@
 package de.btu.openinfra.backend.db.pojos;
 
 import java.util.List;
+import java.util.UUID;
 
 import de.btu.openinfra.backend.db.pojos.project.AttributeValuePojo;
 
+/**
+ * Only use this class in combination with a attribute type group since the
+ * attributeTypeToAttributeTypeGroupId relates to a specific attribute type
+ * group.
+ *
+ * @author <a href="http://www.b-tu.de">BTU</a> DBIS
+ *
+ */
 public class AttributeTypeToValue {
 
+	private UUID attributeTypeToAttributeTypeGroupId;
 	private AttributeTypePojo attributeType;
 	private List<AttributeValuePojo> attributeValues;
 
@@ -13,9 +23,12 @@ public class AttributeTypeToValue {
 
 	public AttributeTypeToValue(
 			AttributeTypePojo attributeType,
-			List<AttributeValuePojo> attributeValues) {
+			List<AttributeValuePojo> attributeValues,
+			UUID attributeTypeToAttributeTypeGroupId) {
 		this.attributeType = attributeType;
 		this.attributeValues = attributeValues;
+		this.attributeTypeToAttributeTypeGroupId =
+				attributeTypeToAttributeTypeGroupId;
 	}
 
 	public AttributeTypePojo getAttributeType() {
@@ -32,6 +45,15 @@ public class AttributeTypeToValue {
 
 	public void setAttributeValues(List<AttributeValuePojo> attributeValues) {
 		this.attributeValues = attributeValues;
+	}
+
+	public UUID getAttributeTypeToAttributeTypeGroupId() {
+		return attributeTypeToAttributeTypeGroupId;
+	}
+
+	public void setAttributeTypeToAttributeTypeGroupId(
+			UUID attributeTypeToAttributeTypeGroupId) {
+		this.attributeTypeToAttributeTypeGroupId = attributeTypeToAttributeTypeGroupId;
 	}
 
 }
