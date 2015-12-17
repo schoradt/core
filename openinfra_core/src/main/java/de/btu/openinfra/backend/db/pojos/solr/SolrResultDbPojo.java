7a50bc275e1f52b1ed457e62b5446bdc8ba18218
xa2kx <schulal2@b-tu.de>
2015-12-17 11:16:00 +0100

    added meta data for topic characteristic and instance to search results

--- openinfra_core/src/main/java/de/btu/openinfra/backend/db/pojos/solr/SolrResultDbPojo.java
+++ openinfra_core/src/main/java/de/btu/openinfra/backend/db/pojos/solr/SolrResultDbPojo.java
@@ -4,14 +4,15 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+import de.btu.openinfra.backend.db.pojos.MetaDataPojo;
 import de.btu.openinfra.backend.db.pojos.OpenInfraPojo;
 
 /**
  * This POJO is a container for a database result of a Solr query.
- *
- * TODO The POJOs must extend OpenInfraPojo to be accessible for the primer
- *      class. The UUID and TRID that is provided by the OpenInfraPojo will
- *      never be used.
+ * <br/><br/>
+ * Note: The POJOs must extend OpenInfraPojo to be accessible for the primer
+ *       class. The UUID and TRID that is provided by the OpenInfraPojo will
+ *       never be used.
  *
  * @author <a href="http://www.b-tu.de">BTU</a> DBIS
  */
@@ -20,6 +21,8 @@ public class SolrResultDbPojo extends OpenInfraPojo {
     private UUID topicInstanceId;
     private UUID topicCharacteristicId;
     private UUID projectId;
+    private MetaDataPojo topicCharacteristicMetaData;
+    private MetaDataPojo topicInstanceMetaData;
     private Map<String, List<String>> highlight;
 
     public UUID getProjectId() {
@@ -44,6 +47,23 @@ public class SolrResultDbPojo extends OpenInfraPojo {
 
     public void setTopicCharacteristicId(UUID topicCharacteristicId) {
         this.topicCharacteristicId = topicCharacteristicId;
+    }
+
+    public MetaDataPojo getTopicCharacteristicMetaData() {
+        return topicCharacteristicMetaData;
+    }
+
+    public void setTopicCharacteristicMetaData(
+            MetaDataPojo topicCharacteristicMetaData) {
+        this.topicCharacteristicMetaData = topicCharacteristicMetaData;
+    }
+
+    public MetaDataPojo getTopicInstanceMetaData() {
+        return topicInstanceMetaData;
+    }
+
+    public void setTopicInstanceMetaData(MetaDataPojo topicInstanceMetaData) {
+        this.topicInstanceMetaData = topicInstanceMetaData;
     }
 
     public Map<String, List<String>> getHighlight() {
