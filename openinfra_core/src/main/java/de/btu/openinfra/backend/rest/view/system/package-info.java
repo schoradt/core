fa036c85bdcd82272fb022caa2ef862cf7b42e11
xa2kx <schulal2@b-tu.de>
2015-12-22 11:09:44 +0100

    removed JSP files and other associated files

--- openinfra_core/src/main/java/de/btu/openinfra/backend/rest/view/system/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * This is a very technical GUI in order to test details of the system schema.
- *
- * @author <a href="http://www.b-tu.de">BTU</a> DBIS
- *
- */
-package de.btu.openinfra.backend.rest.view.system;
